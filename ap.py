8e07bd7324054b24af797a0e0cd68caa6c075498
Rohit-Yadav07 <89621826+Rohit-Yadav07@users.noreply.github.com>
2024-11-26 00:16:32 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 import streamlit as st
-from streamlit_option_menu import option_menu
+# from streamlit_option_menu import option_menu
 
 # Set page configuration
 st.set_page_config(page_title="Healthcare Multi-Disease Diagnosis",
